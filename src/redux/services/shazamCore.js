3d5ed7ab7c4b6b0f254d6ca23555b419382a6cb4
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-09-24 17:35:13 +0200

    1st Commit
    init
    structure of project
    main getApi with store and display

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '9c1794096emsh848522f328ab106p10a9abjsn19af098d6f71',
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
